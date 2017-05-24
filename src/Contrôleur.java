46605d1468bdf80fbf60166522d0ba5f8a4f3d76
maniert <maniert@pc-dg-039-08>
2017-05-24 14:57:28 +0200

    initialisation complete

--- src/Contrôleur.java
+++ src/Contrôleur.java
@@ -1,68 +1,65 @@
+
 import java.util.*;
 
 public class Contrôleur {
 
-	Grille grille;
-	Collection<Aventurier> aventuriers;
-	TasCartesTrésor cartesTresorEnJeu;
-	Collection<TasCartesTrésor> cartesTresor;
-	Collection<TasCartesInnondation> cartesInnondation;
-	Collection<TasCartesInnondation> cartesInnondationsHorsjeu;
+    Grille grille;
+    Collection<Aventurier> aventuriers;
+    TasCartesTrésor cartesTresorEnJeu;
+    Collection<TasCartesTrésor> cartesTresor;
+    Collection<TasCartesInnondation> cartesInnondation;
+    Collection<TasCartesInnondation> cartesInnondationsHorsjeu;
 
-	/**
-	 * 
-	 * @param a
-	 */
-	public void jouerTour(Aventurier a) {
-		// TODO - implement Contrôleur.jouerTour
-		throw new UnsupportedOperationException();
-	}
+    /**
+     *
+     * @param a
+     */
+    public void jouerTour(Aventurier a) {
+        // TODO - implement Contrôleur.jouerTour
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param a
-	 */
-	public void realiserAction(Aventurier a) {
-		// TODO - implement Contrôleur.realiserAction
-		throw new UnsupportedOperationException();
-	}
+    /**
+     *
+     * @param a
+     */
+    public void realiserAction(Aventurier a) {
+        // TODO - implement Contrôleur.realiserAction
+        throw new UnsupportedOperationException();
+    }
 
-	public void monterEchelleEau() {
-		// TODO - implement Contrôleur.monterEchelleEau
-		throw new UnsupportedOperationException();
-	}
+    public void monterEchelleEau() {
+        // TODO - implement Contrôleur.monterEchelleEau
+        throw new UnsupportedOperationException();
+    }
 
-	public void getPosxy() {
-		// TODO - implement Contrôleur.getPosxy
-		throw new UnsupportedOperationException();
-	}
+    /**
+     *
+     * @param a
+     */
+    public void seDeplacer(Aventurier a) {
+        // TODO - implement Contrôleur.seDeplacer
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param a
-	 */
-	public void seDeplacer(Aventurier a) {
-		// TODO - implement Contrôleur.seDeplacer
-		throw new UnsupportedOperationException();
-	}
+    /**
+     *
+     * @param aventurier
+     * @param carte
+     */
+    public void defausserCarteTresorJoueur(Aventurier aventurier, Ctrésor carte) {
+        // TODO - implement Contrôleur.defausserCarteTresorJoueur
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param aventurier
-	 * @param carte
-	 */
-	public void defausserCarteTresorJoueur(Aventurier aventurier, Ctrésor carte) {
-		// TODO - implement Contrôleur.defausserCarteTresorJoueur
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param carteS
-	 */
-	public void utiliserCarteSpeciale(CSpéciale carteS) {
-		// TODO - implement Contrôleur.utiliserCarteSpeciale
-		throw new UnsupportedOperationException();
-	}
+    /**
+     *
+     * @param aventurier
+     * @param carteS
+     */
+    public void utiliserCarteSpeciale(Aventurier aventurier, CSpéciale carteS) {
+        // TODO - implement Contrôleur.utiliserCarteSpeciale
+        throw new UnsupportedOperationException();
+    }
 
 }
