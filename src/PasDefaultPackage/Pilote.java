ac6d1b6027a5b89fb22057bb4ad3a89b58b58b90
Alexis Renaux <imareek26@gmail.com>
2017-05-29 11:50:56 +0200

    Merge origin/master
    Conflicts:
    	src/PasDefaultPackage/Aventurier.java

--- src/PasDefaultPackage/Pilote.java
+++ src/PasDefaultPackage/Pilote.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Pilote extends Aventurier{
     
-    public Pilote(TypeRole typeRole, int idaventurier, Tuile t, ArrayList<Carte> main) {
+    public Pilote(String nomJoueur, TypeRole typeRole, int idaventurier, Tuile t, ArrayList<Carte> main) {
         super(typeRole, idaventurier, t, main);
     }
     
